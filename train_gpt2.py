Set weight initialization with correct scaling

train_gpt2.py
@@ -32,6 +32,7 @@ class CausalSelfAttention(nn.Module):
         # k,q,x projections for all heads
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOSCALE_GPT_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
@@ -94,6 +95,22 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+        
+        # init params
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            if hasattr(module, 'NANOSCALE_GPT_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.2)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
@@ -164,6 +181,38 @@ class GPT(nn.Module):
         return model
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------
+import tiktoken
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        
+        # at init load tokens from disk and store in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B,T)
+        y = (buf[1:]).view(B,T)
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y 
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
 num_return_sequences = 5
 max_length = 30
 
@@ -175,33 +224,29 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"Using device: {device}")
 
+torch.manual_seed(1337)
+if torch.cpu.is_available():
+    torch.cuda.manual_seed(1337)
 
-#get a databatch
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T+1])
-buf = buf.to(device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B,T)
+train_loader = DataLoaderLite(B=16, T=1024)
+
+torch.set_float32_matmul_precision('high')
 
 # get logits 
 model = GPT(GPTConfig()) # vocab_size=50257, block_size=1024))
 model.to(device)
-logits, loss = model(x, targets=y)
 
 # optimizer
 optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
-for i in range(50):
+for i in range(5000):
+    x,y = train_loader.next_batch()
+    x,y = x.to(device), y.to(device)
     optimizer.zero_grad() # set gradients to 0
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step() # update parameters and decrease loss
-    print(f"step {i}, loss: {loss.item()}")
+    if i % 100 == 0:
+        print(f"step {i}, loss: {loss.item()}")
 
 print(loss)
 import sys; sys.exit(0)