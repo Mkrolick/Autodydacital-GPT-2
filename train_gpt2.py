736d4847c1b79e27e0a3b9b7d5d99d63bb928136
Malcolm Krolick <malcolm.krolick@gmail.com>
2025-02-08 12:04:56 -0500

    Hotfix
    - Fix naming
    - Fix standard deviation in embedding & linear layers

--- train_gpt2.py
+++ train_gpt2.py
@@ -104,13 +104,14 @@ class GPT(nn.Module):
     
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
+            std = 0.02
             if hasattr(module, 'NANOSCALE_GPT_INIT'):
                 std *= (2 * self.config.n_layer) ** -0.5
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.2)
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
-            torch.nn.normal_(module.weight, mean=0.0, std=0.02)
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
