2c56ec27d1a5447b892716fe9d8f9e00c071ed44
Malcolm Krolick <malcolm.krolick@gmail.com>
2025-02-13 16:23:29 -0500

    Added Live Predictions!

--- train_gpt2.py
+++ train_gpt2.py
@@ -427,6 +427,47 @@ for step in range(max_steps):
                 # save model to folder 
                 torch.save(checkpoint, checkpoint_path)
 
+    # predict sentences occasionally
+    if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
+        model.eval()
+        num_return_sequences = 4
+        max_len = 32
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode("Hello, I'm a language model,")
+        tokens = torch.tensor(tokens, dtype=torch.long)
+        tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+
+        # inline generated sequence
+        x = tokens.to(device)
+        # create a generator object inline to not interfere with global generator object
+        sample_rng = torch.Generator(device=device)
+        sample_rng.manual_seed(42 + ddp_rank)
+        while x.size(1) < max_len:
+            # forward model to get logits
+            with torch.no_grad():
+                logits = model(x) # (B, T, vocab_size)
+                # take logits at last position
+                logits = logits[:, -1, :] # (B, vocab_size)
+                # get probabilities
+                probs = F.softmax(logits, dim=-1)
+                # do top-k sampling of 50 (huggingface pipeline default)
+                # topk_probs here becomes (5, 50), topk_indivies is (5, 50)
+                topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+                # select a token from top-k probabilities
+                ix = torch.multinomial(topk_probs, 1) # (B, 1)
+                # gather the corresponding indices
+                xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+                # append to sequence
+                x = torch.cat((x,xcol), dim=1)
+
+        # print generated text
+        for i in range(num_return_sequences):
+            tokens = x[i, :max_len].tolist()
+            decoded = enc.decode(tokens)
+            print(">", decoded)
+        
+            
+
 
     # Optional: Add hellaswag eval code
 
@@ -481,49 +522,3 @@ if master_process:
 
 if ddp: 
     destroy_process_group()
-
-import sys; sys.exit(0)
-
-
-
-#model = GPT.from_pretrained('gpt2')
-model = GPT(GPTConfig(vocab_size=50257, block_size=1024))
-model.eval()
-model.to(device) # for use on lambda compute
-print("didn't crash yay")
-
-# prefix tokens
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to(device)
-print(x)
-
-# generate! right now x is (B,T) where B = 5, T = 8
-# set the seed to 42
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-while x.size(1) < max_length:
-    # forward model to get logits
-    with torch.no_grad():
-        logits = model(x) # (B, T, vocab_size)
-        # take logits at last position
-        logits = logits[:, -1, :] # (B, vocab_size)
-        # get probabilities
-        probs = F.softmax(logits, dim=-1)
-        # do top-k sampling of 50 (huggingface pipeline default)
-        # topk_probs here becomes (5, 50), topk_indivies is (5, 50)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-        # select a token from top-k probabilities
-        ix = torch.multinomial(topk_probs, 1) # (B, 1)
-        # gather the corresponding indices
-        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
-        # append to sequence
-        x = torch.cat((x,xcol), dim=1)
-
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
