0a9d81f52bece7f4ba927bde5c035c4d7d49e294
Malcolm Krolick <malcolm.krolick@gmail.com>
2025-02-12 21:37:07 -0500

    Preparing for CUDA run

--- train_gpt2.py
+++ train_gpt2.py
@@ -6,6 +6,7 @@ from torch.nn import functional as F
 import math
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
+import time
 
 batch_size = 64
 block_size = 256
@@ -192,7 +193,7 @@ class GPT(nn.Module):
     def configure_optimizers(self, weight_decay, learning_rate, device):
         # start with all of the candidate parameters (that require grad)
         param_dict =  {pn: p for pn, p in self.named_parameters()}
-        param_dict = {pn: p for pn, p in param_dict.item() if p.requires_grad}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
         # create optim options. Any paramter that is 2D will be weight destroyed, otherwise no.
         # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
         decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
@@ -204,7 +205,7 @@ class GPT(nn.Module):
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
         print(f"num decayed params tensor: {len(decay_params)}, with {num_decay_params:,} parameters")
-        print(f"num non-decayed params tensor: {len(num_nodecay_params)}, with {num_nodecay_params:,} parameters")
+        print(f"num non-decayed params tensor: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
         # Create AdamW optimizer and use the fused version if it is available
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and 'cuda' in device
@@ -214,13 +215,48 @@ class GPT(nn.Module):
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------
 import tiktoken
+import numpy as np
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = npt.astype(np.int32)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+
+
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank=0, num_processes=1):
+    def __init__(self, B, T, process_rank=0, num_processes=1, split="train"):
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.num_processes = num_processes
+        self.split = split
+        assert split in {'train', 'val'}
+
+        # get shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split  in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        self.reset()
         
+    
+    def reset(self):
+        # state innit at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+        
+
+        """
+        Code for shakespeare lite
+
         # at init load tokens from disk and store in memory
         with open('input.txt', 'r') as f:
             text = f.read()
@@ -232,6 +268,7 @@ class DataLoaderLite:
 
         # state
         self.current_position = self.B * self.T * self.process_rank
+        """
 
     def next_batch(self):
         B, T = self.B, self.T
@@ -247,8 +284,7 @@ class DataLoaderLite:
 
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------
-num_return_sequences = 5
-max_length = 30
+# Simple launch script: torchrun --standalone --nproc_per_node=8 train_gpt2.py
 
 # set default device
 device = "cpu"
@@ -290,7 +326,7 @@ if torch.cpu.is_available():
 
 # gradient accumulation
 total_batch_size = 524288 # 2**19, ~0.5M in number of tokens
-B = 16 # micro batch size
+B = 16 # micro batch size - check if this should be 64 for A100
 T = 1024 # sequence length
 assert total_batch_size % (B * T * ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T"
 grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
@@ -307,23 +343,25 @@ import sys; sys.exit(0)
 """
 
 
-train_loader = DataLoaderLite(B=8, T=1024, process_rank=ddp_rank, num_processes=ddp_world_size)
+train_loader = DataLoaderLite(B=8, T=1024, process_rank=ddp_rank, num_processes=ddp_world_size, split="train")
+val_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="val")
 
 torch.set_float32_matmul_precision('high')
 
 # create model
 model = GPT(GPTConfig(vocab_size=50304)) # vocab_size=50257, block_size=1024))
 model.to(device)
-if device == 'cuda':
-    torch.compile(device)
+use_compile = False
+if device == 'cuda' and use_compile:
+    torch.compile(model)
 if ddp:
     model = DDP(model, device_ids=[ddp_local_rank])
 raw_model = model.module if ddp else model # always contain the "raw" unwrapped module
 
 max_lr = 6e-4
 min_lr = max_lr * 0.1
-warmup_steps = 10
-max_steps = 50
+warmup_steps = 715 # 375e6 / 2^19 == 715 warmup steps
+max_steps = 19073 # 19073 # 10e9 / 2^19 (tokens per sec)
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
     if it < warmup_steps:
@@ -340,18 +378,55 @@ def get_lr(it):
 # optimize!
 #torch.optim.Adam(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 optimizer = raw_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
-for step in range(max_steps):
-    optimizer.zero_grad() # set gradients to 0
 
+
+# creat the log directory where we will write checkpoints to
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, f"log.txt")
+with open(log_file, "w") as f:
+    pass
+
+
+
+for step in range(max_steps):
+    t0 = time.time()
+    last_step = (step == max_steps - 1)
+    
+    # Once in a while updates validation loss
+    if step % 100 == 0:
+        model.eval()
+        val_loader.reset()
+        with torch.no_grad():
+            val_loss_accum = 0.0
+            val_loss_steps = 20
+            for _ in range(val_loss_steps):
+                x, y = val_loader.next_batch()
+                x, y = x.to(device), y.to(device)
+                # uncomment for CUDA run
+                # with torch.autocast(device_type=device, dtype=torch.bfloat16):
+                logits, loss = model(x,y)
+                loss = loss / val_loss_steps
+                val_loss_accum += loss.detach()
+        if ddp:
+            dist.all_reduce(val_loss_accum, op=dist.ReduceOp.AVG)
+        if master_process:
+            print(f"validation loss: {val_loss_accum.item():.4f}")
+
+
+    # Optional: Add hellaswag eval code
+
+    # Optimization step
+    model.train()
+    optimizer.zero_grad() # set gradients to 0
     loss_accum = 0.0
-    for micro_step in grad_accum_steps:
+    for micro_step in range(grad_accum_steps):
         x,y = train_loader.next_batch()
         x,y = x.to(device), y.to(device) 
 
-        # add if device != mps
-        
-        with torch.autocast(device_type=device, dtype=torch.bfloat16):
-            logits, loss = model(x, y)
+        # Uncomment for CUDA:
+        # with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
 
         loss = loss / grad_accum_steps
         loss_accum += loss.detach()
@@ -369,10 +444,30 @@ for step in range(max_steps):
     optimizer.step() # update parameters and decrease loss
 
     torch.cuda.synchronize()
+    
+    t1 = time.time()
+    dt = t1 - t0
+    tokens_process = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
+    tokens_per_sec = tokens_process / dt
     if master_process: 
-        print(f"step {step:4d}, loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f}")
+        print(f"step {step:4d}, loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000} | tokens/sec: {tokens_per_sec}")
+        with open(log_file, "a") as f:
+            f.write(f"{step} train {loss_accum.item():.6f}\n")
 
-print(loss)
+# Save finalized model
+if master_process:
+    checkpoint = {
+        'model': raw_model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'step': step,
+        'config': raw_model.config,
+    }
+    print(f"saving checkpoint to {log_dir}")
+    torch.save(checkpoint, os.path.join(log_dir, 'ckpt.pt'))
+
+if ddp: 
+    destroy_process_group()
+
 import sys; sys.exit(0)
 
 
